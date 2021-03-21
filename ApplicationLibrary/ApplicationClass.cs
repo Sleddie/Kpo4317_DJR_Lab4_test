fb64863750da62f142ce546ca6a6f462f42be8e0
Sleddie <jdelmuhametov@gmail.com>
2021-03-22 00:35:10 +0300

    Initial commit

---
+++ ApplicationLibrary/ApplicationClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLibrary
+{
+    public class ApplicationClass
+    {
+        public string DirectRun(string initial_data)
+        {
+            string status = "in progress";
+
+            try
+            {
+                Console.WriteLine("Hello, " + initial_data);
+                status = "success";
+            }
+            catch (Exception)
+            {
+                status = "fail";
+            }
+
+            return status;
+        }
+
+        public string RunLauncher()
+        {
+            string status = "in progress";
+
+            try
+            {
+                string initial_data = "guest. Current status - " + status;
+                status = DirectRun(initial_data);
+            }
+            catch (Exception)
+            {
+                status = "fail";
+            }
+
+            return status;
+        }
+
+        public double Hypotenuse(double first_leg, double second_leg)
+        {
+            return Math.Sqrt(Math.Pow(first_leg, 2) + Math.Pow(second_leg, 2));
+        }
+
+        public char SymbolFromCode(int ascii_code)
+        {
+            char symbol = Convert.ToChar(ascii_code);
+
+            return symbol;
+        }
+
+        public int HowmuchSeconds(int[] time) // 0] years; 1] days; 2] hours; 3] minutes; 4] seconds.
+        {
+            int days = time[0] * 365 + time[1];
+            int hours = days * 24 + time[2];
+            int minutes = hours * 60 + time[3];
+            int seconds = minutes * 60 + time[4];
+
+            return seconds;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ApplicationLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodRun()
+        {
+            var target = new ApplicationClass();
+
+            string current_status = target.DirectRun("Jamil");
+            string expected_status = "success";
+            Assert.AreEqual(expected_status, current_status);
+        }
+
+        [TestMethod]
+        public void TestLauncherRun()
+        {
+            var target = new ApplicationClass();
+
+            string current_status = target.RunLauncher();
+            string expected_status = "success";
+            Assert.AreEqual(expected_status, current_status);
+        }
+
+        [TestMethod]
+        public void HypotenuseCalculatingTest()
+        {
+            var target = new ApplicationClass();
+
+            double expected = 5.0;
+            double actual = target.Hypotenuse(3.0, 4.0);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecoderTest()
+        {
+            var target = new ApplicationClass();
+
+            char expected = 'A';
+            char actual = target.SymbolFromCode(65);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToSecondsTest()
+        {
+            var target = new ApplicationClass();
+
+            int expected = 645782463;
+            int[] time_array = new int[5] { 20, 174, 8, 1, 3 };
+            int actual = target.HowmuchSeconds(time_array);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
